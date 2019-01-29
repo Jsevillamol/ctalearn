dc3702123d06f324cdc92f2a84ec6536e160c543
Jsevillamol <jsevillamol@outlook.com>
2019-01-29 12:20:47 +0100

    Summarize multiple runs

--- ctalearn/summarize.py
+++ ctalearn/summarize.py
@@ -7,8 +7,8 @@ Created on Sun Dec 30 18:19:55 2018
 """
 
 import os
-import csv
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
@@ -17,46 +17,51 @@ def summarize_multi_results():
     train_dirs = [dir_ for dir_ in os.listdir() if os.path.isdir(dir_)]
     train_dirs.sort()
     
-    # Read the results from the runs
-    histories = []
-    summary_csv_fn = 'multi_summary.csv'
+    # Read the results from the runs    
     first_iteration = True
     for train_dir in train_dirs:
         run_csv_fn = f'{train_dir}/training_history.csv'
         try:
-            with open(run_csv_fn) as csv_file:
-                csv_reader = csv.reader(csv_file)
-                csv_contents = []
-                for row in csv_reader: 
-                    csv_contents.append(row)
-                if len(csv_contents) == 0: continue
-                metric_names = csv_contents[0]
-                history = np.array(csv_contents[1:])
-                histories.append(history)
-        except FileNotFoundError: continue # If there is no training_history.csv we ignore this folder
+            run_df = pd.read_csv(run_csv_fn)
+            run_df['run_name'] = train_dir
+            
+        except (FileNotFoundError,pd.errors.EmptyDataError): 
+            continue # If there is no training_history.csv we ignore this folder
         
-        # Write the names of the columns in the first iteration
         if first_iteration:
-            with open(summary_csv_fn, 'w', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile)
-                csv_writer.writerow(['run_name'] + metric_names)
+            metric_names = list(run_df)
+            metric_names.remove('run_name')
+            metric_names.remove('epoch')
+            summary_df = run_df
             first_iteration = False
+        else:
+            summary_df = pd.concat([summary_df, run_df])
     
-        # Write the final results in a summary file
-        with open(summary_csv_fn, 'a', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow([train_dir] + list(history[-1, :]))
-        
-    # Plot the runs
-    for i, metric in enumerate(metric_names):
+    # Save summary
+    summary_csv_fn = 'multi_summary.csv'
+    summary_df.to_csv(summary_csv_fn)
+    
+    # Plot the training progress per metric of each run
+    for metric in metric_names:
+        #plt.legend()
         plt.title(f'model {metric}')
         plt.ylabel(metric)
         plt.xlabel('epoch')
-        for history in histories: 
-            hist = list(map(float, history[:, i]))
-            plt.plot(hist)
+        
+        for label, run_df in summary_df.groupby('run_name'):
+            plt.plot(run_df[metric])
+        
         plt.savefig(f'history_{metric}.png')
         plt.clf()
+    
+    # plot a histogram of the number of epochs per run
+    n_epochs = summary_df.groupby('run_name').size().values
+    bins = np.arange(0, n_epochs.max() + 1.5)
+    fig, ax = plt.subplots()
+    ax.hist(n_epochs, bins)
+    ax.set_xticks(bins)
+    plt.savefig('n_epochs.png')
+    plt.clf()
     
             
 ########################
